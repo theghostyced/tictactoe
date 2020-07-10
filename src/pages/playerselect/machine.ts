59f3236a882b257b6a8248413fd0d0c485b1f83e
Daniel Eze <dan4allu93@gmail.com>
2020-07-10 21:26:12 +0100

    creating the player select machine

--- src/pages/playerselect/machine.ts
+++ src/pages/playerselect/machine.ts
@@ -1,14 +1,37 @@
-import { createMachine } from 'xstate';
+import { assign, createMachine } from 'xstate';
 
 export const PlayerSelectMachineStates = {
-  PLAYER_X: 'X',
-  PLAYER_O: 'O'
+  PLAYER_SELECT: 'PLAYER_SELECT',
+  PLAYER_SELECTED: 'PLAYER_SELECTED',
 }
 
-const PlayerSelectMachine = createMachine({
+export interface PlayerSelectContext {
+  player: string | null;
+}
+
+export interface PlayerEvent {
+  type: string;
+  player_type: string | null;
+}
+
+const PlayerSelectMachine = createMachine<PlayerSelectContext>({
   id: 'playerselect',
+  initial: PlayerSelectMachineStates.PLAYER_SELECT.toLowerCase(),
   context: {
     player: null
+  },
+  states: {
+    player_select: {
+      on: {
+        [PlayerSelectMachineStates.PLAYER_SELECTED]: {
+          target: PlayerSelectMachineStates.PLAYER_SELECTED.toLowerCase(),
+          actions: assign({
+            player: (_, event: PlayerEvent) => event.player_type
+          })
+        }
+      }
+    },
+    player_selected: {}
   }
 });
 
